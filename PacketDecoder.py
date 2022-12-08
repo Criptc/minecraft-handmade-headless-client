1053b939d434faa4adb7c7b3749e3a1cb4329efd
Criptc <96071765+Criptc@users.noreply.github.com>
2022-12-08 14:40:57 -0600

    added login disconnect in client, untested

--- PacketDecoder.py
+++ PacketDecoder.py
@@ -1,8 +1,22 @@
 import uuid
 import struct
+import json
 
 
-def _static_unpack_varint(data):
+def _static_pack_varint(val):
+	total = b''
+	if val < 0:
+		val = (1<<32)+val
+	while val>=0x80:
+		bits = val&0x7F
+		val >>= 7
+		total += struct.pack('B', (0x80|bits))
+	bits = val&0x7F
+	total += struct.pack('B', bits)
+	return total
+
+
+def _static_unpack_varint(data, return_num=False):
     total = 0
     shift = 0
     val = 0x80
@@ -14,7 +28,10 @@ def _static_unpack_varint(data):
         n += 1
     if total & (1 << 31):
         total = total - (1 << 32)
-    return total
+    if return_num:
+        return total, n
+    else:
+        return total
 
 
 class _Packets:
@@ -43,7 +60,6 @@ class _Packets:
                     if login_suc_bytes[i:i+1].decode().isprintable():
                         username.append(login_suc_bytes[i:i+1].decode())
                     else:
-                        print(i)
                         break
                 except Exception:
                     pass
@@ -77,6 +93,12 @@ class _Packets:
 
     class Special:
         @staticmethod
+        def Encryption_Request(packet_data):
+            packet_data = packet_data[20:len(packet_data)]
+            length, num = _static_unpack_varint(packet_data, return_num=True)
+            
+            
+        @staticmethod
         def custom_payload(packet_data):
             packet_data = packet_data[16:len(packet_data)]
             server_type = []
@@ -91,8 +113,77 @@ class _Packets:
                 except Exception:
                     pass
             return ''.join(server_type)
+    class client:
+        @staticmethod
+        def handshake(packet_data):
+            protocol_version, n = _static_unpack_varint(packet_data, return_num=True)
+            packet_data = packet_data[n:len(packet_data)]
+            if packet_data.endswith(b'\x01'):
+                return protocol_version, False
+            else:
+                return protocol_version, True
 
+        @staticmethod
+        def loginstart(packet_data):
+            username = []
+            for i in range(len(packet_data)):
+                try:
+                    if packet_data[i:i+1].decode().isprintable():
+                        username.append(packet_data[i:i+1].decode())
+                    else:
+                        break
+                except:
+                    pass
+            username = "".join(username)
+            packet_data = packet_data[len(username):len(packet_data)]
+            sig_data = struct.unpack_from('?', packet_data)
+            packet_data = packet_data[1:len(packet_data)]
+            if sig_data:
+                timestamp = struct.unpack_from('l', packet_data)
+                length = len(struct.pack('l', timestamp))
+                packet_data = packet_data[length:len(packet_data)]
+                pubkeylen, n = _static_unpack_varint(packet_data, return_num=True)
+                packet_data = packet_data[1:len(packet_data)]
+                pubkey = struct.unpack_from('c'*pubkeylen, packet_data)
+                packet_data = packet_data[pubkeylen:len(packet_data)]
+                signiturelen, n = _static_unpack_varint(packet_data, return_num=True)
+                packet_data = packet_data[1:len(packet_data)]
+                signiture = struct.unpack_from('c'*signiturelen, packet_data)
+                packet_data = packet_data[signiturelen:len(packet_data)]
 
+            hasUUID = struct.unpack_from('?', packet_data)
+            if hasUUID:
+                packet_data = packet_data[1:len(packet_data)]
+                puuid = _Packets.Tools.decode_uuid(packet_data)
+
+            if sig_data:
+                if hasUUID:
+                    return {"username": username, "sigdata": True, "timestamp": timestamp, "publickey": pubkey, "signiture": signiture, "hasUUID": True, "UUID": puuid}
+                else:
+                    return {"username": username, "sigdata": True, "timestamp": timestamp, "publickey": pubkey, "signiture": signiture, "hasUUID": False}
+            else:
+                if hasUUID:
+                    return {"username": username, "sigdata": False, "hasUUID": True, "UUID": puuid}
+                else:
+                    return {"username": username, "sigdata": False, "hasUUID": False}
+
+        @staticmethod
+        def disconnect(reason, bold=False, italic=False, underlined=False, strikethrough=False):
+            packet = b"\x00"
+
+            json_data = {
+                "text": reason,
+                "bold": bold,
+                "italic": italic,
+                "underlined": underlined,
+                "strikethrough": strikethrough
+            }
+
+            packet += json.dumps(json_data)
+            packet = _static_pack_varint(len(packet)) + packet
+            return packet
+            
+                
 packet = {
     "decode": {
         "login": {
